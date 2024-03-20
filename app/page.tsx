0e8992c725ea3b614340005ef3159b0780063b8e
thientantran <thientantm@gmail.com>
2024-03-20 12:18:03 +0700

    Installation and Setup project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-green-500 text-3xl font-bold">Hello world</p>
+      <Button variant='destructive'>Click me</Button>
+    </div>
+
+  );
+}
