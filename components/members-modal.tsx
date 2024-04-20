317f3e0e66a8bb40bde38d9e2f7f66bf6dbe135d
thientantran <thientantm@gmail.com>
2024-04-20 14:52:29 +0700

    Manage Members 4: Change role API

--- components/members-modal.tsx
+++ components/members-modal.tsx
@@ -7,7 +7,10 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { useModalStore } from "@/hooks/useModalStore";
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
-import { Check, Gavel, MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion } from "lucide-react";
+import axios from "axios";
+import { Check, Gavel, Loader2, MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion } from "lucide-react";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
 import { useState } from "react";
 
 const roleIconMap = {
@@ -17,12 +20,32 @@ const roleIconMap = {
 }
 
 export default function MembersModal() {
+  const router = useRouter()
   const { onOpen, isOpen, onClose, type, data } = useModalStore();
   const isModalOpen = isOpen && type === 'members';
 
   const { server } = data as { server: ServerWithMembersWithProfiles }
 
   const [loadingId, setLoadingId] = useState("")
+
+  const onChangeRole = async (memberId: string, role: MemberRole) => {
+    try {
+      setLoadingId(memberId)
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server.id,
+        }
+      })
+      const response = await axios.patch(url, { role })
+      router.refresh()
+      onOpen('members', { server: response.data })
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoadingId("")
+    }
+  }
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black overflow-hidden">
@@ -61,14 +84,14 @@ export default function MembersModal() {
                         </DropdownMenuSubTrigger>
                         <DropdownMenuPortal>
                           <DropdownMenuSubContent>
-                            <DropdownMenuItem className="cursor-pointer">
+                            <DropdownMenuItem onClick={() => onChangeRole(member.id, MemberRole.GUEST)} className="cursor-pointer">
                               <Shield className="h-4 w-4 mr-2" />
                               Guest
                               {member.role === MemberRole.GUEST && (
                                 <Check className="h-4 w-4 ml-auto" />)
                               }
                             </DropdownMenuItem>
-                            <DropdownMenuItem className="cursor-pointer">
+                            <DropdownMenuItem onClick={() => onChangeRole(member.id, MemberRole.MODERATOR)} className="cursor-pointer">
                               <ShieldCheck className="h-4 w-4 mr-2" />
                               Moderator
                               {member.role === MemberRole.MODERATOR && (
@@ -87,9 +110,9 @@ export default function MembersModal() {
                   </DropdownMenu>
                 </div>
               )}
-              {/* {loadingId === member.id && (
+              {loadingId === member.id && (
                 <Loader2 className="animate-spin text-zinc-500 ml-auto w-4 h-4" />)
-              } */}
+              }
             </div>
           ))}
         </ScrollArea>
