7261391acd834ad9c69969d08bea432c4cf8dc3e
thientantran <thientantm@gmail.com>
2024-04-20 14:03:53 +0700

    Manage Members 2: Binding members to modal

--- components/members-modal.tsx
+++ components/members-modal.tsx
@@ -1,28 +1,52 @@
 'use client'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
+import UserAvatar from "@/components/UserAvatar";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useModalStore } from "@/hooks/useModalStore";
+import { ServerWithMembersWithProfiles } from "@/types";
+import { MemberRole } from "@prisma/client";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
 
+const roleIconMap = {
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 text-rose-500" />,
+  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 text-indigo-500" />,
+  [MemberRole.GUEST]: '',
+}
 
 export default function MembersModal() {
   const { onOpen, isOpen, onClose, type, data } = useModalStore();
   const isModalOpen = isOpen && type === 'members';
 
-  const { server } = data
-
-
+  const { server } = data as { server: ServerWithMembersWithProfiles }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+      <DialogContent className="bg-white text-black overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
             Manage Members
           </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} members
+          </DialogDescription>
         </DialogHeader>
-        <div className="p-6">
-          Members
-        </div>
+        <ScrollArea>
+          {server?.members?.map((member) => (
+            <div key={member.id} className="flex items-center gap-x-2 mb-6">
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="text-xs font-semibold flex items-center gap-x-1">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className="text-xs text-zinc-500">
+                  {member.profile.email}
+                </p>
+              </div>
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   )
