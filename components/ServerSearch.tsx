f086c8e69c9102c262210512c32d94bf6db99ca9
thientantran <thientantm@gmail.com>
2024-04-21 12:12:24 +0700

    Search members and channels: Binding and use shadcnui command to binding and searh

---
+++ components/ServerSearch.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { Search } from "lucide-react";
+import React, { useState } from "react";
+
+interface ServerSearchProps {
+  data: {
+    label: string;
+    type: "channel" | "member";
+    data: {
+      icon: React.ReactNode;
+      name: string;
+      id: string;
+    }[] | undefined
+  }[]
+}
+
+export default function ServerSearch({ data }: ServerSearchProps) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <button onClick={() => setOpen(true)} className="group hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition px-2 py-2 rounded-md flex items-center gap-x-2 w-full">
+        <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transistion" />
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transistion">
+          Search
+        </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">CMD</span>K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all channels and members" />
+        <CommandList>
+          <CommandEmpty>
+            No Results found
+          </CommandEmpty>
+          {data.map(({ label, type, data }) => {
+            if (!data?.length) return null
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data.map(({ icon, name, id }) => {
+                  return (
+                    <CommandItem key={id}>
+                      {icon}
+                      <span>{name}</span>
+                    </CommandItem>
+                  )
+                })}
+              </CommandGroup>
+            )
+          })}
+        </CommandList>
+      </CommandDialog>
+    </>
+  )
+}
