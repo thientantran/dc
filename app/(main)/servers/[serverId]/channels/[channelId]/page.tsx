da35ca16131f3c49e8e0bd24d1464b19597c3347
thientantran <thientantm@gmail.com>
2024-04-22 23:57:41 +0700

    Chat header: Layout and binding channel name

--- app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,47 @@
+import ChatHeader from "@/components/ChatHeader";
+import { currentProfile } from "@/lib/current-profile";
+import prismadb from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function page() {
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  }
+}
+
+export default async function page({
+  params
+}: ChannelIdPageProps
+) {
+
+  const profile = await currentProfile()
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  const channel = await prismadb.channel.findUnique({
+    where: {
+      id: params.channelId,
+      serverId: params.serverId
+    }
+  })
+
+  // dung findFirst thay vi findUnique vi minh dung 2 tham so ma ko unique trong members
+  const member = await prismadb.member.findFirst({
+    where: {
+      profileId: profile.id,
+      serverId: params.serverId
+    }
+  })
+
+  if (!channel || !member) {
+    return redirect("/")
+  }
   return (
-    <div>page</div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader serverId={params.serverId} name={channel.name} type="channel" />
+    </div>
   )
 }
